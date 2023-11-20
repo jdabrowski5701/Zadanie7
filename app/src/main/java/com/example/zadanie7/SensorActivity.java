7e97f3fb076f3c10458ce25b57fcd3fb1f087429
Jakub Dąbrowski <jakubdabrowski5701@gmail.com>
2023-11-20 12:45:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/zadanie7/SensorActivity.java
@@ -0,0 +1,14 @@
+package com.example.zadanie7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SensorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sensor_activity);
+    }
+}
